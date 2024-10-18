114e2f9a7758edbedcadeca11ed416a9ad2baa28
WeStOn2000 <161762685+WeStOn2000@users.noreply.github.com>
2024-10-17 20:48:17 -0400

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
-function App() {
+import { useState } from 'react';
+
+function Counter() {
+  // Define the state for the count
+  let [count, setCount] = useState(0);
 
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
+    <div className='text-center'>
+      <div className='flex justify-center items-center'>
+        {/* Button with hover effect */}
+        <button
+          className='bg-blue-500 text-white py-2 px-4 rounded mr-4 hover:bg-blue-700'
+          onClick={() => setCount(count + 1)}
+        >
+          CLICK ME
+        </button>
+        {/* Display the count */}
+        <p className='text-2xl'>{count}</p>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default Counter;
